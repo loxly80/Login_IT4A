63d2e1d992d7c26a9a9e1fa965833e6e8155a5f4
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-10-10 11:35:11 +0200

    hash

--- Login_IT4A/User.cs
+++ Login_IT4A/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Login_IT4A
 {
@@ -10,6 +11,8 @@ namespace Login_IT4A
     {
         public string Name { get; }
         public string Password { get; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
         public User(string name, string password)
         {
@@ -17,9 +20,18 @@ namespace Login_IT4A
             Password = password;
         }
 
-        internal bool VerifyPassword(string text)
+        public bool VerifyPassword(string text)
         {
             return Password == text;
+        }
+
+        public void CreatePasswordHash(string password)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                PasswordSalt = hmac.Key;
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
